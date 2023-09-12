8d9c5f1fb62cc10e6f46eb0cedfe94284a048cf6
Stepan <91005125+Stepan-Borchuk@users.noreply.github.com>
2023-09-12 22:35:18 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,74 +1,85 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 // import PropTypes from 'prop-types';
 import { Box } from './Box';
 import FeedbackOptions from './Buttons';
 import Statistics from './Statistic';
 
-class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleFeedback = name => {
+    switch (name) {
+      case 'good':
+        setGood(prevState => prevState + 1);
+        break;
+
+      case 'neutral':
+        setNeutral(prevState => prevState + 1);
+        break;
+      case 'bad':
+        setBad(prevState => prevState + 1);
+        break;
+
+      default:
+        return;
+    }
   };
 
-  handleFeedback = value => {
-    this.setState(prevState => {
-      return { [value]: prevState[value] + 1 };
-    });
-  };
+  const total = good + neutral + bad;
 
-  totalFeedback = () => {
-    const { good, neutral, bad } = this.state;
-    return good + neutral + bad;
-  };
-
-  positiveFeedback = () => {
-    const total = this.totalFeedback();
-    const { good } = this.state;
+  const positiveFeedback = () => {
     return total !== 0 ? Math.round((good * 100) / total) : 0;
   };
 
-  render() {
-    const buttonsArray = Object.keys(this.state);
-    const statsArray = Object.entries(this.state);
-    const total = this.totalFeedback();
-    const value = this.positiveFeedback();
-    return (
-      <Box
-        as="div"
-        width="30%"
-        display="flex"
-        alignItems="center"
-        justifyContent="left"
-        flexDirection="column"
-        color="accent"
-        ml="auto"
-        mr="auto"
-        bg="background"
-        borderRadius="normal"
-        boxShadow="4px 11px 49px 1px #d7dead"
-      >
-        <Box as="section">
-          <Box font-size="l" display="flex" as="h1">
-            Будь-ласка залиште коментар
-          </Box>
-          <FeedbackOptions
-            options={buttonsArray}
-            onLeaveFeedback={this.handleFeedback}
-          />
+  const buttonsArray = ['good', 'neutral', 'bad'];
 
-          <Box as="section" display="flex" flexDirection="column" width="100%">
-            <h2>Статистика</h2>
-            {total === 0 ? (
-              <h3>No feedback given</h3>
-            ) : (
-              <Statistics total={total} good={value} statsArray={statsArray} />
-            )}
-          </Box>
+  const statsArray = [
+    ['good', good],
+    ['neutral', neutral],
+    ['bad', bad],
+  ];
+
+  return (
+    <Box
+      as="div"
+      width="30%"
+      display="flex"
+      alignItems="center"
+      justifyContent="left"
+      flexDirection="column"
+      color="accent"
+      ml="auto"
+      mr="auto"
+      bg="background"
+      borderRadius="normal"
+      boxShadow="4px 11px 49px 1px #d7dead"
+    >
+      <Box as="section">
+        <Box font-size="l" display="flex" as="h1">
+          Будь-ласка залиште коментар
+        </Box>
+        <FeedbackOptions
+          options={buttonsArray}
+          onLeaveFeedback={handleFeedback}
+        />
+
+        <Box as="section" display="flex" flexDirection="column" width="100%">
+          <h2>Статистика</h2>
+          {total === 0 ? (
+            <h3>No feedback given</h3>
+          ) : (
+            <Statistics
+              total={total}
+              good={positiveFeedback()}
+              statsArray={statsArray}
+            />
+          )}
         </Box>
       </Box>
-    );
-  }
-}
+    </Box>
+  );
+};
 
 export default App;
